e15fe5e367e0d1f57b33409cf1e410c7ecf1c86f
Rodrigo Coelho <rodrigocoeio@gmail.com>
2023-03-05 21:29:55 -0300

    Added Finished Screen Added Shake cards on not matched Added play card button Colors Ajustments Fixed some bugs

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -45,9 +45,11 @@ const Game: FC<GameProps> = (props: GameProps) => {
   const [cardOpenedName, setCardOpenedName] = useState("");
   const [cardOpened, setCardOpened] = useState(0);
   const [cardMatch, setCardMatch] = useState(0);
-  const [finsihed, setFinished] = useState(false);
+  const [cardShake, setCardShake] = useState(false);
+  const [finished, setFinished] = useState(false);
 
   const playAudioHandler = (card: Card) => {
+    if (cardOpened && cardMatch) return false;
     playCard(card);
   };
 
@@ -55,11 +57,14 @@ const Game: FC<GameProps> = (props: GameProps) => {
     setCardOpened(0);
     setCardMatch(0);
     setCardsMatched([]);
+    setFinished(false);
     props.onStartGame();
   };
 
-  const openCardHandler = (card: Card) => {
+  const openCardHandler = (event: any, card: Card) => {
     if (cardOpened && cardMatch) return false;
+
+    playCard(card);
 
     // Open Match Card
     if (cardOpened) {
@@ -75,8 +80,13 @@ const Game: FC<GameProps> = (props: GameProps) => {
         // Not Matched
       } else {
         setTimeout(() => {
+          setCardShake(true);
+        }, 2500);
+
+        setTimeout(() => {
           setCardOpened(0);
           setCardMatch(0);
+          setCardShake(false);
         }, 3000);
         playNotMatched();
       }
@@ -97,7 +107,10 @@ const Game: FC<GameProps> = (props: GameProps) => {
         }, 2000);
       }
     }
+  };
 
+  const playCardHandler = (e: any, card: Card) => {
+    e.stopPropagation();
     playCard(card);
   };
 
@@ -108,10 +121,34 @@ const Game: FC<GameProps> = (props: GameProps) => {
     coverStyles += " " + styles.Clickable + " " + styles.Selectable;
   }
 
+  if (cardShake) {
+    cardStyles += " " + styles.Shake;
+  }
+
   return (
     <div className={styles.Wrapper}>
+      {finished && (
+        <div className={styles.Finished}>
+          <img src="/images/congrats.png"></img>
+          <br />
+          Game Finished!
+          <br />
+          Wanna play more?!
+          <br />
+          <button onClick={newGameHandler}>Yes!</button>
+          <button
+            onClick={() => {
+              setFinished(false);
+            }}
+            style={{ opacity: 0.6 }}
+          >
+            No!
+          </button>
+        </div>
+      )}
       <div
         className={styles.GameBoard + " " + styles[`GameBoard${difficulty}`]}
+        style={{ opacity: finished ? 0.3 : 1 }}
       >
         {cardsSet.map((card, index) => {
           const indexer = index + 1;
@@ -143,35 +180,51 @@ const Game: FC<GameProps> = (props: GameProps) => {
             <div
               key={`cover${indexer}`}
               className={coverStyles}
-              onClick={() => openCardHandler(card)}
+              onClick={(e) => openCardHandler(e, card)}
             >
-              {indexer}
+              <span>{indexer}</span>
+              <button
+                onClick={(e) => playCardHandler(e, card)}
+                className={styles.Play}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  className="bi bi-speaker-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M9 4a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm-2.5 6.5a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0z" />
+                  <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm6 4a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM8 7a3.5 3.5 0 1 1 0 7 3.5 3.5 0 0 1 0-7z" />
+                </svg>
+              </button>
             </div>
           );
         })}
-      </div>
 
-      <div className={styles.Controls}>
-        <button onClick={newGameHandler} className={styles.NewGame}>
-          New Game&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        </button>
-        <button
-          onClick={props.onQuitGame}
-          className={styles.QuitGame}
-          title="Quit Game"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="36"
-            height="36"
-            fill="currentColor"
-            className="bi bi-x-circle"
-            viewBox="0 0 16 16"
+        <div className={styles.Controls}>
+          <button onClick={newGameHandler} className={styles.NewGame}>
+            New Game&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </button>
+          <button
+            onClick={props.onQuitGame}
+            className={styles.QuitGame}
+            title="Quit Game"
           >
-            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-          </svg>
-        </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="36"
+              height="36"
+              fill="currentColor"
+              className="bi bi-x-circle"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   );
